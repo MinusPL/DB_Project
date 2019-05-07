d2eefe948101e8fc82236728870af48ad4cabc30
MinusPL <theminusoid@gmail.com>
2019-05-07 20:28:23 +0200

    Update settings.py

--- elearning/settings.py
+++ elearning/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-import python_dotenv
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
