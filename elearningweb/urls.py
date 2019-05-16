df9779051f09c53133ce6c9e4e360231cc708706
Piotr Materna <piotr_materna@protonmail.com>
2019-05-16 21:25:56 +0200

    Test1

--- elearningweb/urls.py
+++ elearningweb/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('courses/', views.CoursesView.as_view(), name='courses'),
-    path('tests/', views.CoursesView.as_view(), name='tests')
+    path('tests/', views.TestsView.as_view(), name='tests')
 ]
