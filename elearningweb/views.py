97bcd4d69e48eb5ce915969262d08bd1bb31eda3
Piotr Materna <piotr_materna@protonmail.com>
2019-05-16 22:37:37 +0200

    test2

--- elearningweb/views.py
+++ elearningweb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from dbhandler.models import Course, Test
+from dbhandler.models import Course, Test, Answer, Question
 
 # Create your views here.
 
@@ -17,4 +17,9 @@ class HomeView(TemplateView):
 
 class TestsView(ListView):
     model = Test
+    def get_context_data(self, **kwargs):
+        context = super(TestsView, self).get_context_data(**kwargs)
+        context['questions'] = Question.objects.all()
+        context['answers'] = Answer.objects.all()
+        return context
     template_name = 'tests.html'
