4bf0a41fdc4b8e144439cf6cc69f1664fe27ab05
Piotr Materna <piotr_materna@protonmail.com>
2019-05-17 18:21:52 +0200

    CreateTest AddQuestion

--- elearningweb/urls.py
+++ elearningweb/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('courses/', views.CoursesView.as_view(), name='courses'),
-    path('tests/', views.TestsView.as_view(), name='tests')
+    path('tests/', views.TestsView.as_view(), name='tests'),
+    path('createtest/', views.CreateTestView.as_view(),name='createtest'),
+    path('addquestions/', views.AddQuestionsView.as_view(),name='addquestions')
 ]
--- elearningweb/views.py
+++ elearningweb/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from dbhandler.models import Course, Test, Answer, Question
+from dbhandler.models import Course, Test, Answer, Question, Class
 
 # Create your views here.
 
@@ -16,10 +16,53 @@ class HomeView(TemplateView):
     template_name = 'index.html'
 
 class TestsView(ListView):
-    model = Test
     def get_context_data(self, **kwargs):
         context = super(TestsView, self).get_context_data(**kwargs)
         context['questions'] = Question.objects.all()
         context['answers'] = Answer.objects.all()
         return context
     template_name = 'tests.html'
+        
+class CreateTestView(TemplateView):
+    model = Class
+    template_name = 'createTest.html'
+    def post(self, request, *args, **kwargs):
+        testName = request.POST['testname']
+        testDesc = request.POST['testdesc']
+        #ClassId = request.POST['classid']
+        t=Test(name=testName,description=testDesc,class_id_id=2)
+        t.save()
+        return redirect('addQuestions')
+
+class AddQuestionsView(TemplateView):
+    template_name = 'addQuestions.html'
+    def post(self, request, *args, **kwargs):
+        qText = request.POST['content']
+        ans = [
+            request.POST['answer1'],
+            request.POST['answer2'],
+            request.POST['answer3'],
+            request.POST['answer4']
+        ]
+        corAns = request.POST['isCorrect']
+        #ClassId = request.POST['classid']
+        q=Question(question_text=qText,test_id_id=5)
+        q.save()
+        i = 1
+        for a in ans:
+            if i==corAns:
+                a=Answer(answer_text=a,is_good=1,question_id_id=q.id)
+            else:
+                a=Answer(answer_text=a,is_good=0,question_id_id=q.id)
+            a.save()
+            i = i+1
+        return redirect('addquestions')
+
+class ManageTestView(TemplateView):
+    template_name = 'manageTest.html'
+    def get_context_data(self, **kwargs):
+        context = super(TestsView, self).get_context_data(**kwargs)
+        context['questions'] = Question.objects.all()
+        context['answers'] = Answer.objects.all()
+        return context
+ #   def post(self, request, *args, **kwargs):
