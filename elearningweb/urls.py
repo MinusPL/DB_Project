74d806f4249eb101177b91ecc14061113d30b532
chg7 <chg7@protonmail.ch>
2019-05-18 11:12:12 +0200

    Dodano podstronę wybranego kursu

--- elearningweb/urls.py
+++ elearningweb/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('courses/', views.CoursesView.as_view(), name='courses'),
+    path('courses/<int:pk>', views.CourseDetailView.as_view(), name='course_detail'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('register/', views.RegisterView.as_view(), name='register')
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
