fcb51774989deaeeb06fa12529f85f2f672f96eb
al1cja <48214594+al1cja@users.noreply.github.com>
2019-05-25 17:48:15 +0200

    Dodano widok zajęć

--- elearningweb/urls.py
+++ elearningweb/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('courses/', views.CoursesView.as_view(), name='courses'),
     path('courses/<int:pk>', views.CourseDetailView.as_view(), name='course_detail'),
+    path('courses/<int:pk>/<int:class.id>', views.ClassesView.as_view(), name='class'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('register/', views.RegisterView.as_view(), name='register'),
+ 
 ]
--- elearningweb/views.py
+++ elearningweb/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from dbhandler.models import Course
+from dbhandler.models import Class
 
 # Create your views here.
 
@@ -16,6 +17,10 @@ class CourseDetailView(DetailView):
 	model = Course
 	template_name = 'course_detail.html'
 
+class ClassesView(DetailView):
+    model = Class
+    template_name = 'class.html'
+
 class HomeView(TemplateView):
     template_name = 'index.html'
 
@@ -24,3 +29,4 @@ class LoginView(TemplateView):
 
 class RegisterView(TemplateView):
     template_name = 'register.html'
+
