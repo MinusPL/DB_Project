c80b5afd4a1c5b63bb1f9f3ae4fd8bb7c7b7dce5
al1cja <48214594+al1cja@users.noreply.github.com>
2019-05-26 01:21:11 +0200

    Dodano obsługę dodawania komentarzy

--- elearningweb/views.py
+++ elearningweb/views.py
@@ -46,6 +46,13 @@ class CourseDetailView(DetailView):
 
 class ClassesView(DetailView):
     model = Class
+    def post(self, request, *args, **kwargs):
+        comment = request.POST['comment'],
+        author = request.POST['author'],
+        id = request.POST['id']
+        c=Comment(text=comment, author_id=author, class_id=id )
+        c.save()
+        return redirect('../class/%d' % c.id)            
     template_name = 'class.html'
 
 class HomeView(TemplateView):
