f6eb2897e1afd1da5f1edde93cfd318a3edc1098
Piotr Materna <piotr_materna@protonmail.com>
2019-05-26 21:27:39 +0200

    Revert "Revert "Testy - sprawdzanie sesji""
    This reverts commit 4556edd916baeed1a77d03f98a8f9c024a08fbfe.

--- dbhandler/models.py
+++ dbhandler/models.py
@@ -63,6 +63,7 @@ class Answer(models.Model):
     question_id = models.ForeignKey('Question', on_delete=models.CASCADE)
 
 class TestResult(models.Model):
-    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
-    test = models.ForeignKey('Test', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    test_id = models.ForeignKey('Test', on_delete=models.CASCADE)
     result = models.IntegerField()
+    maxScore = models.IntegerField()
