f440648250bb1b1588cdb048d760364c316ed11f
chg7 <chg7@protonmail.ch>
2019-05-29 12:31:39 +0200

    Reset hasła przez email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
+    path('password_reset', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
